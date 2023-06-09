cd461bfa64bd8c777d41639ab22210630da4cf85
Oleh Sytnik <osyt@telenor.dk>
2023-06-10 00:11:20 +0300

    NewConsoleDi added

---
+++ NewConsoleDi/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace NewConsoleDi;
+
+public record Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public int Age { get; set; }
+    public string Gender { get; set; } = null!;
+    public string? Address { get; set; }
+}
---
+++ NewConsoleDi/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewConsoleDi;
+
+var provider = ConfigureServices();
+await using var context = provider.GetRequiredService<SomeDataContext>();
+var array = await GetUserNames(context);
+foreach (var name in array) Console.WriteLine(name);
+Console.ReadKey();
+
+static ServiceProvider ConfigureServices()
+{
+    var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+    var collection = new ServiceCollection();
+    collection.AddDbContext<SomeDataContext>(builder => builder.UseSqlServer(connectionString));
+    return collection.BuildServiceProvider();
+}
+
+Task<string[]> GetUserNames(SomeDataContext dataContext) =>
+    dataContext.Persons.Select(person => $"{person.FirstName} {person.LastName}").ToArrayAsync();
