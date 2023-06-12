e78d7247dece70e1f425ec2ec08f3fa4e5030a8e
Oleh Sytnik <osyt@telenor.dk>
2023-06-12 15:59:48 +0300

    dbcontext case

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,31 +1,26 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using BlazorApp;
 using BlazorApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpContextAccessor();
+// var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContextFactory<SomeDataContextOnConfiguring>();
+builder.Services.AddScoped(provider =>
+    provider.GetService<IDbContextFactory<SomeDataContextOnConfiguring>>().CreateDbContext());
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
